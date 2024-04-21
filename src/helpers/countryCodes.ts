f4f9b0eba7ff3447311b3af453371fa96bc72626
Stefano Antonel <stefanoantonel@gmail.com>
2024-04-21 18:33:34 +0200

    Change name of endpoint to get ISO country code

--- src/helpers/countryCodes.ts
+++ src/helpers/countryCodes.ts
@@ -2,7 +2,7 @@ import config from '@/config.json';
 
 export function getCountryCodeISO(countryCode: string, signal?: AbortSignal) {
   // get from "DE" to "DEU" to fetch country border
-  const endpoint = config.countryNameEndpoint.replace('<COUNTRY_CODE>', countryCode);
+  const endpoint = config.countryISOCodeEndpoint.replace('<COUNTRY_CODE>', countryCode);
 
   return fetch(endpoint, { signal })
     .then((res) => res.text())
