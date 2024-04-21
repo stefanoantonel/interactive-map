0fd8d8ac9e553ff5868552174e68f25c928a9d51
stefanoantonel <stefanoantonel@users.noreply.github.com>
2024-04-21 22:16:50 +0200

    env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {unoptimized: true},
+  basePath,
 };
 
 export default nextConfig;
