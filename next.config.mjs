46819a9cae0fd935c88a213dae89efbebeca736f
Stefano Antonel <stefanoantonel@gmail.com>
2024-04-21 22:19:17 +0200

    lint

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {unoptimized: true},
+  images: { unoptimized: true },
   basePath,
 };
 
