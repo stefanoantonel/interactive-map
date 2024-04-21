bc9b927d7e704f76adfd503d280f89c4b6f42044
Stefano Antonel <stefanoantonel@gmail.com>
2024-04-21 22:28:03 +0200

    Scroll to top

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
+/** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
   basePath,
 };
 
